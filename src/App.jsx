639c652e0f5639dbd0d8f74bc58f2fd79a4e6cdc
Programacion <jorgeluishuergo88@gmail.com>
2022-05-23 20:45:18 -0500

    feat: project structure

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Blog from "./pages/Blog";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import './App.css'
+
+
+const App = () =>(
+
+  
+      <div className="container">
+        <Routes>
+          <Route path='/' exact element={<Home />}/>
+          <Route path='/blog/:id' element={<Blog />}/>
+        </Routes>
+      </div>
+  );
+
+
+export default App;
